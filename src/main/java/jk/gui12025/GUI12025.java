e921e36f1a0a525f9ebe9c2f3b4af9bc15dad78e
Kamiri <kamiritech01@gmail.com>
2025-09-25 17:32:09 +0300

    Initial commit

---
+++ src/main/java/jk/gui12025/GUI12025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jk.gui12025;
+
+/**
+ *
+ * @author user
+ */
+public class GUI12025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
